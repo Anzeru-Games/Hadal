dea9d3d036aef65e9b1fd10bf1b6f9b5e1a3a87b
Anzeru <gregb7865@gmail.com>
2025-03-31 13:01:46 -0400

    Revert "Update index.js"
    This reverts commit c7dc949c2b9721e15faa4be8b17f85896eb0df91.

--- index.js
+++ index.js
@@ -1,69 +1,69 @@
-createCard('CompressedMedia/Hadal 3_26_2025 9_38_06 PM.png', 'Additional tweaks to stylization, increased thickness of edge, decreased intensity of normal maps.', 'March 26, 2025');
-createCard('CompressedMedia/Hadal 3_23_2025 10_12_54 PM.png', 'Added some cool pillars, and a grounding force (keeps player stuck to slopes when moving on them).', 'March 23, 2025');
-createCard('CompressedMedia/Hadal 3_23_2025 3_49_10 PM.png', 'A better example of the edge stylization that was added.', 'March 23, 2025');
-createCard('CompressedMedia/Screenshot 2025-03-22 223705.png', 'Kinda forgot to update this, but I\'ve added enemy AI, parrying, proper weapon switching, health bars, light edge stylization, made gun model take up more screen real estate.', 'March 22, 2025');
-createCard('CompressedMedia/Screenshot 2025-03-22 223717.png', 'Here, the game interface was further refined, including some UI tweaks to make the experience smoother, and a better feel of the world itself through dynamic lighting.', 'March 22, 2025');
-createCard('CompressedMedia/Screenshot 2025-03-22 223802.png', 'Death screen and all the new particle effects.', 'March 22, 2025');
-createCard('CompressedMedia/Screenshot 2025-03-22 224018.png', 'Focus on more visual updates, with improved textures and shading for the environment, along with some additional environmental effects that enhance atmosphere.', 'March 22, 2025');
+createCard('CompressedMedia/Hadal 3_26_2025 9_38_06 PM.png', 'Additional tweaks to stylization, increased thickness of edge, decreased intensity of normal maps.', 'March 2025');
+  createCard('CompressedMedia/Hadal 3_23_2025 10_12_54 PM.png', 'Added some cool pillars, and a grounding force (keeps player stuck to slopes when moving on them).', 'March 2025');
+  createCard('CompressedMedia/Hadal 3_23_2025 3_49_10 PM.png', 'A better example of the edge stylization that was added.', 'March 2025');
+  createCard('CompressedMedia/Screenshot 2025-03-22 223705.png', 'Kinda forgot to update this, but I\'ve added enemy AI, parrying, proper weapon switching, health bars, light edge stylization, made gun model take up more screen real estate.', 'March 2025');
+  createCard('CompressedMedia/Screenshot 2025-03-22 223717.png', 'Here, the game interface was further refined, including some UI tweaks to make the experience smoother, and a better feel of the world itself through dynamic lighting.', 'March 2025');
+  createCard('CompressedMedia/Screenshot 2025-03-22 223802.png', 'Death screen and all the new particle effects.', 'March 2025');
+  createCard('CompressedMedia/Screenshot 2025-03-22 224018.png', 'Focus on more visual updates, with improved textures and shading for the environment, along with some additional environmental effects that enhance atmosphere.', 'March 2025');
 
-createCard('CompressedMedia/Hadal 1_27_2025 9_37_43 PM.png', 'Implemented hands with overlay camera in URP.', 'January 27, 2025');
-createCard('CompressedMedia/Hadal 1_27_2025 9_37_55 PM.png', 'This image demonstrates the inclusion of a first-person perspective with hands visible, providing immersion for the player in the world with integrated URP settings.', 'January 27, 2025');
+  createCard('CompressedMedia/Hadal 1_27_2025 9_37_43 PM.png', 'Implemented hands with overlay camera in URP.', 'January 2025');
+  createCard('CompressedMedia/Hadal 1_27_2025 9_37_55 PM.png', 'This image demonstrates the inclusion of a first-person perspective with hands visible, providing immersion for the player in the world with integrated URP settings.', 'January 2025');
 
-createCard('CompressedMedia/Hadal 1_24_2025 10_26_30 AM.png', 'Implemented a rock ground attack, does no damage currently, but does knockback and looks cool.', 'January 24, 2025');
-createCard('CompressedMedia/Hadal 1_24_2025 10_26_31 AM.png', 'The new attack feature was added, with visual feedback for knockback and ground interaction, though damage still needs to be incorporated.', 'January 24, 2025');
-createCard('CompressedMedia/Hadal 1_24_2025 10_26_33 AM.png', 'The updated attack animation for the rock ground feature was refined, making it more dynamic and responsive during gameplay.', 'January 24, 2025');
+  createCard('CompressedMedia/Hadal 1_24_2025 10_26_30 AM.png', 'Implemented a rock ground attack, does no damage currently, but does knockback and looks cool.', 'January 2025');
+  createCard('CompressedMedia/Hadal 1_24_2025 10_26_31 AM.png', 'The new attack feature was added, with visual feedback for knockback and ground interaction, though damage still needs to be incorporated.', 'January 2025');
+  createCard('CompressedMedia/Hadal 1_24_2025 10_26_33 AM.png', 'The updated attack animation for the rock ground feature was refined, making it more dynamic and responsive during gameplay.', 'January 2025');
 
-createCard('CompressedMedia/Hadal_Dash.gif', 'Testing a stylization with pixelation, added dashing and improved movement.', 'December 2024');
-createCard('CompressedMedia/Hadal 11_4_2024 10_36_42 PM.png', 'Testing a stylization dither shader, might be a little extreme on the color banding and such for now.', 'November 4, 2024');
-createCard('CompressedMedia/OceanGameTestURP 11_2_2024 11_59_03 PM.png', 'Added coral details to the arena, provided by Quixel.', 'November 2, 2024');
-createCard('CompressedMedia/OceanGameTestURP 11_2_2024 11_58_54 PM.png', 'Updated underwater elements for better immersion, integrating more detail into the environment to enhance the game\'s aesthetics.', 'November 2, 2024');
-createCard('CompressedMedia/OceanGameTestURP 11_2_2024 11_58_47 PM.png', 'Refinement of the lighting effects, adding subtle refractions and enhancing the feel of being underwater.', 'November 2, 2024');
-createCard('CompressedMedia/OceanGameTestURP 11_1_2024 2_33_04 PM.png', 'Complete transition to the URP version of Unity 6, boosting performance by a significant margin. Brightened the scene in general to make it look less bleak.', 'November 1, 2024');
-createCard('CompressedMedia/OceanGameTestURP 11_1_2024 2_33_11 PM.png', 'Started tweaking the terrain shaders to better match the aesthetic of the overall underwater theme and environment.', 'November 1, 2024');
-createCard('CompressedMedia/OceanGameTestURP 11_1_2024 2_33_17 PM.png', 'Further optimizations for performance, especially with handling underwater objects and visibility in complex scenes.', 'November 1, 2024');
-createCard('CompressedMedia/OceanGameTestURP 11_1_2024 2_33_24 PM.png', 'An additional test of the underwater effects, with the focus on water distortion and visual coherence.', 'November 1, 2024');
-createCard('CompressedMedia/OceanGameTestURP 11_1_2024 2_33_32 PM.png', 'Underwater environment that includes better water transparency, wave effects, and improved object lighting and shadows.', 'November 1, 2024');
+  createCard('CompressedMedia/Hadal_Dash.gif', 'Testing a stylization with pixelation, added dashing and improved movement.', 'December 2024');
+  createCard('CompressedMedia/Hadal 11_4_2024 10_36_42 PM.png', 'Testing a stylization dither shader, might be a little extreme on the color banding and such for now.', 'November 2024');
+  createCard('CompressedMedia/OceanGameTestURP 11_2_2024 11_59_03 PM.png', 'Added coral details to the arena, provided by Quixel.', 'November 2024');
+  createCard('CompressedMedia/OceanGameTestURP 11_2_2024 11_58_54 PM.png', 'Updated underwater elements for better immersion, integrating more detail into the environment to enhance the game\'s aesthetics.', 'November 2024');
+  createCard('CompressedMedia/OceanGameTestURP 11_2_2024 11_58_47 PM.png', 'Refinement of the lighting effects, adding subtle refractions and enhancing the feel of being underwater.', 'November 2024');
+  createCard('CompressedMedia/OceanGameTestURP 11_1_2024 2_33_04 PM.png', 'Complete transition to the URP version of Unity 6, boosting performance by a significant margin. Brightened the scene in general to make it look less bleak.', 'November 2024');
+  createCard('CompressedMedia/OceanGameTestURP 11_1_2024 2_33_11 PM.png', 'Started tweaking the terrain shaders to better match the aesthetic of the overall underwater theme and environment.', 'November 2024');
+  createCard('CompressedMedia/OceanGameTestURP 11_1_2024 2_33_17 PM.png', 'Further optimizations for performance, especially with handling underwater objects and visibility in complex scenes.', 'November 2024');
+  createCard('CompressedMedia/OceanGameTestURP 11_1_2024 2_33_24 PM.png', 'An additional test of the underwater effects, with the focus on water distortion and visual coherence.', 'November 2024');
+  createCard('CompressedMedia/OceanGameTestURP 11_1_2024 2_33_32 PM.png', 'Underwater environment that includes better water transparency, wave effects, and improved object lighting and shadows.', 'November 2024');
 
-createCard('CompressedMedia/OceanGame 10_26_2024 6_11_22 PM.png', 'Adding different kelp lengths, and grass LODs for performance.', 'October 26, 2024');
-createCard('CompressedMedia/OceanGame 10_26_2024 6_11_30 PM.png', 'More tweaks to foliage and vegetation, optimizing for performance while keeping visual fidelity.', 'October 26, 2024');
-createCard('CompressedMedia/OceanGame 10_26_2024 6_11_42 PM.png', 'Testing LODs and further adjustments to underwater flora, ensuring the environment doesn’t sacrifice performance.', 'October 26, 2024');
-createCard('CompressedMedia/OceanGame 10_26_2024 6_11_59 PM.png', 'The scene now better handles larger amounts of underwater plant life without reducing frame rates or visual quality.', 'October 26, 2024');
-createCard('CompressedMedia/OceanGame 10_26_2024 6_12_16 PM.png', 'Increased variety in aquatic plants to improve visual richness, making the environment feel more diverse and realistic.', 'October 26, 2024');
-createCard('CompressedMedia/OceanGame 10_26_2024 6_12_27 PM.png', 'Enhanced kelp animations and improved grass behavior for more natural movement in the underwater setting.', 'October 26, 2024');
+  createCard('CompressedMedia/OceanGame 10_26_2024 6_11_22 PM.png', 'Adding different kelp lengths, and grass LODs for performance.', 'October 2024');
+  createCard('CompressedMedia/OceanGame 10_26_2024 6_11_30 PM.png', 'More tweaks to foliage and vegetation, optimizing for performance while keeping visual fidelity.', 'October 2024');
+  createCard('CompressedMedia/OceanGame 10_26_2024 6_11_42 PM.png', 'Testing LODs and further adjustments to underwater flora, ensuring the environment doesn’t sacrifice performance.', 'October 2024');
+  createCard('CompressedMedia/OceanGame 10_26_2024 6_11_59 PM.png', 'The scene now better handles larger amounts of underwater plant life without reducing frame rates or visual quality.', 'October 2024');
+  createCard('CompressedMedia/OceanGame 10_26_2024 6_12_16 PM.png', 'Increased variety in aquatic plants to improve visual richness, making the environment feel more diverse and realistic.', 'October 2024');
+  createCard('CompressedMedia/OceanGame 10_26_2024 6_12_27 PM.png', 'Enhanced kelp animations and improved grass behavior for more natural movement in the underwater setting.', 'October 2024');
 
-createCard('CompressedMedia/OceanGame 10_25_2024 7_17_02 PM.png', 'Experimenting with Quixel Megascan assets and boss rooms.', 'October 25, 2024');
-createCard('CompressedMedia/OceanGame 10_25_2024 7_17_40 PM.png', 'Refinement of boss room textures and environmental assets from Quixel, helping to add depth to the game\'s atmosphere.', 'October 25, 2024');
+  createCard('CompressedMedia/OceanGame 10_25_2024 7_17_02 PM.png', 'Experimenting with Quixel Megascan assets and boss rooms.', 'October 2024');
+  createCard('CompressedMedia/OceanGame 10_25_2024 7_17_40 PM.png', 'Refinement of boss room textures and environmental assets from Quixel, helping to add depth to the game\'s atmosphere.', 'October 2024');
 
-createCard('CompressedMedia/OceanGame 5_3_2024 10_09_07 PM.png', 'Experimenting with texturing, modified the kelp, ground cover additions, added crosshair.', 'May 3, 2024');
+  createCard('CompressedMedia/OceanGame 5_3_2024 10_09_07 PM.png', 'Experimenting with texturing, modified the kelp, ground cover additions, added crosshair.', 'May 2024');
 
-createCard('CompressedMedia/FishAndReload.gif', 'Experimenting with reloading animations and weapon view models.', 'April 2024');
+  createCard('CompressedMedia/FishAndReload.gif', 'Experimenting with reloading animations and weapon view models.', 'April 2024');
 
-createCard('CompressedMedia/FishAndWave.gif', 'Adding some arms with IK, view model shader found here: https://forum.unity.com/threads/fps-sample-shader-graph-for-weapon-fov-not-working.576796/', 'April 2024');
+  createCard('CompressedMedia/FishAndWave.gif', 'Adding some arms with IK, view model shader found here: https://forum.unity.com/threads/fps-sample-shader-graph-for-weapon-fov-not-working.576796/', 'April 2024');
 
-createCard('CompressedMedia/OceanGame 4_11_2024 2_06_46 PM.png', 'Flocking fish, courtesy of https://github.com/abecombe/VFXGraphStudy. Had to change the update of the fish rendering away from the fixed update to a regular update loop to remove jitter.', 'April 11, 2024');
-createCard('CompressedMedia/OceanGame 4_6_2024 6_51_46 PM.png', 'Went for a more pixelated stylized look, and I\'m actually really happy with how this turned out.', 'April 6, 2024');
-createCard('CompressedMedia/OceanGame 4_6_2024 6_51_30 PM.png', 'Refinement of the visual style, exploring pixelation to create a more stylized and unique aesthetic for the game.', 'April 6, 2024');
+  createCard('CompressedMedia/OceanGame 4_11_2024 2_06_46 PM.png', 'Flocking fish, courtesy of https://github.com/abecombe/VFXGraphStudy. Had to change the update of the fish rendering away from the fixed update to a regular update loop to remove jitter.', 'April 2024');
+  createCard('CompressedMedia/OceanGame 4_6_2024 6_51_46 PM.png', 'Went for a more pixelated stylized look, and I\'m actually really happy with how this turned out.', 'April 2024');
+  createCard('CompressedMedia/OceanGame 4_6_2024 6_51_30 PM.png', 'Refinement of the visual style, exploring pixelation to create a more stylized and unique aesthetic for the game.', 'April 2024');
 
-createCard('CompressedMedia/OceanGame 4_6_2024 4_55_19 PM.png', 'I\'m still not completely happy with the changes on the water and caustics, but the performance is so many leagues better than the typical HDRP water. It could definitely be possible to have the HDRP water as an option in the settings if the user desires.', 'April 6, 2024');
-createCard('CompressedMedia/OceanGame 4_6_2024 4_55_31 PM.png', 'Experimenting with water shaders, optimizing for performance while maintaining a visually appealing ocean surface.', 'April 6, 2024');
+  createCard('CompressedMedia/OceanGame 4_6_2024 4_55_19 PM.png', 'I\'m Still not completely happy with the changes on the water and caustics, but the performance is so many leagues better than the typical HDRP water. It could definitely be possible to have the HDRP water as an option in the settings if the user desires.', 'April 2024');
+  createCard('CompressedMedia/OceanGame 4_6_2024 4_55_31 PM.png', 'Experimenting with water shaders, optimizing for performance while maintaining a visually appealing ocean surface.', 'April 2024');
 
-createCard('CompressedMedia/OceanGame 4_4_2024 1_55_26 PM.png', 'Kelp is now offset by the world position for the wave function, added a color modifier, and fixed a weird problem with ambient occlusion on the kelp.', 'April 4, 2024');
+  createCard('CompressedMedia/OceanGame 4_4_2024 1_55_26 PM.png', 'Kelp is now offset by the world position for the wave function, added a color modifier, and fixed a weird problem with ambient occlusion on the kelp.', 'April 2024');
 
-createCard('CompressedMedia/OceanGame 4_3_2024 10_47_20 PM.png', 'Testing the kelp shader works for now, but it will need to offset the wave function by world position to make them move differently from each other.', 'April 3, 2024');
-createCard('CompressedMedia/OceanGame 4_3_2024 10_47_28 PM.png', 'Making the kelp shader more responsive to the world position for more fluid motion in the water.', 'April 3, 2024');
+  createCard('CompressedMedia/OceanGame 4_3_2024 10_47_20 PM.png', 'Testing the kelp shader works for now, but it will need to offset the wave function by world position to make them move differently from each other.', 'April 2024');
+  createCard('CompressedMedia/OceanGame 4_3_2024 10_47_28 PM.png', 'Making the kelp shader more responsive to the world position for more fluid motion in the water.', 'April 2024');
 
-createCard('CompressedMedia/OceanGame - OutdoorsScene - Windows, Mac, Linux - Unity 2022.3.20f1 _DX11_ 4_3_2024 4_41_57 PM.png', 'Adding a player controller to the game, is very barebones at this point.', 'April 3, 2024');
+  createCard('CompressedMedia/OceanGame - OutdoorsScene - Windows, Mac, Linux - Unity 2022.3.20f1 _DX11_ 4_3_2024 4_41_57 PM.png', 'Adding a player controller to the game, is very barebones at this point.', 'April 2024');
 
-createCard('CompressedMedia/OceanGame_4_3_2024_12_28_28_AM.png', 'Testing a different foliage method, this time using an opaque rendering method, preventing overdraw on the GPU.', 'April 3, 2024');
+  createCard('CompressedMedia/OceanGame_4_3_2024_12_28_28_AM.png', 'Testing a different foliage method, this time using an opaque rendering method, preventing overdraw on the GPU.', 'April 2024');
 
-createCard('CompressedMedia/OceanGame_4_2_2024_11_11_27_PM.png', 'Testing foliage with VFX graph, this method of drawing the alpha resulted in a ton of overdraw, increasing GPU times. Foam and ripples are added to enhance the ocean surface\'s look from below.', 'April 2, 2024');
+  createCard('CompressedMedia/OceanGame_4_2_2024_11_11_27_PM.png', 'Testing foliage with VFX graph, this method of drawing the alpha resulted in a ton of overdraw, increasing GPU times. Foam and ripples are added to enhance the ocean surface\'s look from below.', 'April 2024');
 
-createCard('CompressedMedia/OceanGame_4_1_2024_11_53_17_PM.png', 'Also testing bubbles and terrain textures.', 'April 1, 2024');
+  createCard('CompressedMedia/OceanGame_4_1_2024_11_53_17_PM.png', 'Also testing bubbles and terrain textures.', 'April 2024');
 
-createCard('CompressedMedia/OceanGame_4_1_2024_11_52_02_PM.png', 'Testing bubbles, terrain textures, and post-processing effects.', 'April 1, 2024');
+  createCard('CompressedMedia/OceanGame_4_1_2024_11_52_02_PM.png', 'Testing bubbles, terrain textures, and post-processing effects.', 'April 2024');
 
-createCard('CompressedMedia/OceanGame_4_1_2024_12_24_37_AM.png', 'Testing HDRP and the art style that is planned, and faking volumetric lighting with VFX graph', 'April 1, 2024');
+  createCard('CompressedMedia/OceanGame_4_1_2024_12_24_37_AM.png', 'Testing HDRP and the art style that is planned, and faking volumetric lighting with VFX graph', 'April 2024');
 
-createCard('CompressedMedia/image1.png', 'URP test, found to be difficult to add caustics to surfaces receiving main light, so URP is cut.', 'March 2024');
+  createCard('CompressedMedia/image1.png', 'URP test, found to be difficult to add caustics to surfaces receiving main light, so URP is cut.', 'March 2024');
 
-createCard('CompressedMedia/image.png', 'HDRP test with volumetric lighting, overall underperformant so volumetrics were cut.', 'March 2024');
+  createCard('CompressedMedia/image.png', 'HDRP test with volumetric lighting, overall underperformant so volumetrics were cut.', 'March 2024');
